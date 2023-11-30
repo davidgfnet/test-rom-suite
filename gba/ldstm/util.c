b5321cdb933e2e15c6bad8350057403ee7d41044
David Guillen Fandos <david@davidgf.net>
2023-12-01 00:07:39 +0100

    Minor cleanup and fixes

--- gba/ldstm/util.c
+++ gba/ldstm/util.c
@@ -7,15 +7,8 @@ static char dec2hex(unsigned x) {
   return 'a' + x - 10;
 }
 
-void ldm_fmt(char *out, unsigned base_reg, unsigned mode, unsigned mask) {
+void ldmstm_fmt(char *out, unsigned base_reg, unsigned mode, unsigned mask) {
   int cnt = 0;
-  out[cnt++] = ' ';
-  out[cnt++] = '>';
-  out[cnt++] = ' ';
-  out[cnt++] = 'L';
-  out[cnt++] = 'D';
-  out[cnt++] = 'M';
-
   out[cnt++] = mode & 2 ? 'D' : 'I';
   out[cnt++] = mode & 4 ? 'A' : 'B';
   out[cnt++] = ' ';
@@ -30,39 +23,32 @@ void ldm_fmt(char *out, unsigned base_reg, unsigned mode, unsigned mask) {
   out[cnt++] = '0';
   out[cnt++] = 'x';
   out[cnt++] = dec2hex(mask >> 12);
-  out[cnt++] = dec2hex((mask >>  8) & 0xF);
-  out[cnt++] = dec2hex((mask >>  4) & 0xF);
+  out[cnt++] = dec2hex((mask >> 8) & 0xF);
+  out[cnt++] = dec2hex((mask >> 4) & 0xF);
   out[cnt++] = dec2hex(mask & 0xF);
   out[cnt++] = 0;
 }
 
 void stm_fmt(char *out, unsigned base_reg, unsigned mode, unsigned mask) {
-  int cnt = 0;
-  out[cnt++] = ' ';
-  out[cnt++] = '>';
-  out[cnt++] = ' ';
-  out[cnt++] = 'S';
-  out[cnt++] = 'T';
-  out[cnt++] = 'M';
+  out[0] = ' ';
+  out[1] = '>';
+  out[2] = ' ';
+  out[3] = 'S';
+  out[4] = 'T';
+  out[5] = 'M';
 
-  out[cnt++] = mode & 2 ? 'D' : 'I';
-  out[cnt++] = mode & 4 ? 'A' : 'B';
-  out[cnt++] = ' ';
-  out[cnt++] = 'r';
-  if (base_reg >= 10)
-    out[cnt++] = '0' + base_reg / 10;
-  out[cnt++] = '0' + base_reg % 10;
+  ldmstm_fmt(&out[6], base_reg, mode, mask);
+}
 
-  out[cnt++] = mode & 1 ? '!' : ' ';
-  out[cnt++] = ',';
-  out[cnt++] = ' ';
-  out[cnt++] = '0';
-  out[cnt++] = 'x';
-  out[cnt++] = dec2hex(mask >> 12);
-  out[cnt++] = dec2hex((mask >>  8) & 0xF);
-  out[cnt++] = dec2hex((mask >>  4) & 0xF);
-  out[cnt++] = dec2hex(mask & 0xF);
-  out[cnt++] = 0;
+void ldm_fmt(char *out, unsigned base_reg, unsigned mode, unsigned mask) {
+  out[0] = ' ';
+  out[1] = '>';
+  out[2] = ' ';
+  out[3] = 'L';
+  out[4] = 'D';
+  out[5] = 'M';
+
+  ldmstm_fmt(&out[6], base_reg, mode, mask);
 }
 
 
